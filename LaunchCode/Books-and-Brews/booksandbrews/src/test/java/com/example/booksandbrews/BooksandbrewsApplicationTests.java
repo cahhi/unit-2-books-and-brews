0e550e06bfe798f8ee62dc116a94a3f2bd56082c
cahhi <chrissyrodgers20@gmail.com>
2025-10-20 12:27:48 -0500

    Initial commit

---
+++ LaunchCode/Books-and-Brews/booksandbrews/src/test/java/com/example/booksandbrews/BooksandbrewsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.booksandbrews;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooksandbrewsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
