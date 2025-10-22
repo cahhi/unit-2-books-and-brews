026bc34e7ae4aa45f8898db597a876b9da65066f
cahhi <chrissyrodgers20@gmail.com>
2025-10-21 20:51:21 -0500

    Completed Book controller and GET/POST tested successfully

--- LaunchCode/unit-2-books-and-brews/java-spring-boot-back-end-app/src/main/java/org/example/java_spring_boot_back_end_app/controllers/BookController.java
+++ LaunchCode/unit-2-books-and-brews/java-spring-boot-back-end-app/src/main/java/org/example/java_spring_boot_back_end_app/controllers/BookController.java
@@ -1,8 +1,35 @@
 package org.example.java_spring_boot_back_end_app.controllers;
 
 
+import org.example.java_spring_boot_back_end_app.data.BookData;
+import org.example.java_spring_boot_back_end_app.models.Book;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.Collection;
+
+@RestController
+@RequestMapping("/api/books") //this is where we tell Spring what the base path is for all the endpoints in this controller
 public class BookController {
+
+    // Retrieve all books
+    // GET http://localhost:8080/api/books
+    @GetMapping("") //no additional path to add
+    public Collection<Book> getAllBooks() {
+        // For demonstration purposes, returning a static list of books
+       return BookData.getAll();
+    }
+
+    //Retrieve a specific book
+
+
+    //Save book
+    //use query parameters
+    @PostMapping("/add")
+    public String addNewBook(@RequestParam String title, String author, int isbn, String description) {
+        Book newBook = new Book(title, author, isbn, description);
+        BookData.addNewBook(newBook);
+        return "Book added successfully: " + newBook;
+    }
 
 
 }
