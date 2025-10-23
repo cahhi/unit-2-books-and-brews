2dfde0791e741fc6884858383ffebddf55e177e8
cahhi <chrissyrodgers20@gmail.com>
2025-10-23 18:32:16 -0500

    Completed User Controller

--- LaunchCode/unit-2-books-and-brews/java-spring-boot-back-end-app/src/main/java/org/example/java_spring_boot_back_end_app/controllers/UserController.java
+++ LaunchCode/unit-2-books-and-brews/java-spring-boot-back-end-app/src/main/java/org/example/java_spring_boot_back_end_app/controllers/UserController.java
@@ -3,9 +3,7 @@ package org.example.java_spring_boot_back_end_app.controllers;
 
 import org.example.java_spring_boot_back_end_app.data.UserData;
 import org.example.java_spring_boot_back_end_app.models.User;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Collection;
 
@@ -21,4 +19,31 @@ public class UserController {
         return UserData.getAll();
     }
 
+    //retrieve a specific user by ID
+    @GetMapping("/details/{userId}")
+    public User getUserById(@PathVariable int userId) {
+        return UserData.getById(userId); //returning object and Spring returns it as a JSON
+    }
+
+    //Retrieve a specific user and return as HTML
+    @GetMapping("/details/{userId}/html") //path variable
+    public String displayUserDetails(@PathVariable int userId) {
+        User user = UserData.getById(userId);
+        return "<h2>User</h2>" + //returning object and Spring returns it
+                "<p><b>ID:</b> " + user.getId() + "</p>" +
+                "<p><b>First Name:</b> " + user.getFirstName() + "</p>" +
+                "<p><b>Last Name:</b> " + user.getLastName() + "</p>" +
+                "<p><b>Email:</b> " + user.getEmail() + "</p>" +
+                "<p><b>Phone Number:</b> " + user.getPhoneNumber() + "</p>" +
+                "<p><b>Address:</b> " + user.getAddress() + "</p>";
+    }
+
+    //Save user
+    //use query parameters
+    @PostMapping("/add")
+    public String addNewUser(@RequestParam String firstName, String lastName, String email, String address, String phoneNumber) {
+        User newUser = new User(firstName, lastName, email, address, phoneNumber);
+        UserData.addNewUser(newUser);
+        return "User added successfully: " + newUser;
+    }
 }
