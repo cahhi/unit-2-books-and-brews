0135abe19eb86fe4d4548eaa280b88a7b475eb55
cahhi <chrissyrodgers20@gmail.com>
2025-11-07 16:41:36 -0600

    Added bookDTO and populated the class

---
+++ LaunchCode/unit-2-books-and-brews/java-spring-boot-back-end-app/src/main/java/org/example/java_spring_boot_back_end_app/dto/BookDTO.java
@@ -0,0 +1,106 @@
+package org.example.java_spring_boot_back_end_app.dto;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.Column;
+import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import org.example.java_spring_boot_back_end_app.models.Author;
+
+public class BookDTO {
+
+    @Column(name="book_title")// I want the column to be named Book title instead of just title
+    @NotBlank(message="Book title is required.") //value can not be blank/null
+    @Size(min=2, max=60, message="Book title must be 2 - 60 characters long.") // creating a min/max character validation
+    private String title;
+
+    //setting the relationship between author and book
+    @ManyToOne //many books to one author
+    @NotBlank(message="Book author is required.")
+    @JsonManagedReference
+    private int authorId;
+
+    @NotBlank(message="Book description is required.")
+    @Size(min=2, max=255, message="Book description must be 2 - 255 characters long.")
+    private String description;
+
+    @NotBlank(message="Book genre is required.")
+    @Size(min=2, max=10, message="Book genre must be 2 - 10 characters long.")
+    private String genre;
+
+    @NotNull(message="Must specify if a book is trending using true/false.")
+    private boolean isTrending;
+
+    @NotNull(message="Book sales price is required.")
+    private float salePrice;
+
+    @NotNull(message="Book original price is required.")
+    private float originalPrice;
+
+    public BookDTO(String title, String description, int authorId, String genre, boolean isTrending, float salePrice, float originalPrice) {
+        this.title = title;
+        this.description = description;
+        this.authorId = authorId;
+        this.genre = genre;
+        this.isTrending = isTrending;
+        this.salePrice = salePrice;
+        this.originalPrice = originalPrice;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getAuthorId() {
+        return authorId;
+    }
+
+    public void setAuthorId(int authorId) {
+        this.authorId = authorId;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public boolean isTrending() {
+        return isTrending;
+    }
+
+    public void setTrending(boolean trending) {
+        isTrending = trending;
+    }
+
+    public float getSalePrice() {
+        return salePrice;
+    }
+
+    public void setSalePrice(float salePrice) {
+        this.salePrice = salePrice;
+    }
+
+    public float getOriginalPrice() {
+        return originalPrice;
+    }
+
+    public void setOriginalPrice(float originalPrice) {
+        this.originalPrice = originalPrice;
+    }
+}
